8d7b1c300f6f870bcd1ea3adf55aead4f3c75f8c
alonc <aloncarme@gmail.com>
2017-03-27 23:27:01 +0300

    initial test

---
+++ SalesTools/src/test/SalesMain.java
@@ -0,0 +1,18 @@
+package test;
+
+public class SalesMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			SalesData data = new SalesData();
+			displayGreeting();
+			data.display();
+			
+			
+	}
+	
+	private static void displayGreeting(){
+		System.out.println("hello");
+		System.out.println("this");
+		}
+}
