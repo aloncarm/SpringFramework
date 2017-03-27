de57459e850598fdb8383e86067a65441c6d5feb
aloncarm <aloncarme@gmail.com>
2017-03-28 00:13:56 +0300

    Update SalesData.java

--- SalesTools/src/test/SalesData.java
+++ SalesTools/src/test/SalesData.java
@@ -7,7 +7,7 @@ public class SalesData {
 	public void display(){
 
 
-System.out.println("ddd3alon11111111111111111111121");
+System.out.println("ddd3alon111111111111111111111551");
 		System.out.println("ddd1");
 
 	}
